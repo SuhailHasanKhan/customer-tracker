2d2093949a15a2255977dfd9ab954d92cab6691e
Suhail Hasan Khan <shasankhan@usf.edu>
2025-09-14 13:57:47 -0400

    Added customers data

--- cc_3.js
+++ cc_3.js
@@ -1,2 +1,19 @@
 // Coding Challenge 3 - Customer Tracker
 
+let customers = [
+    {
+        name: 'Bill Boston',
+        email: 'bostonbill@gmail.com',
+        purchases: ['White Ocean Cologne', 'Ruby Garden Deodorant']
+    },
+    {
+        name: 'Nick Stewart',
+        email: 'nickstewart@gmail.com',
+        purchases: ['Harry Potter Replica Wand', 'Harry Potter Slytherin Robe']
+    },
+    {
+        name: 'Michael Burry',
+        email: 'burrym@gmail.com',
+        purchases: ['Printing Paper', 'Sticky Notes']
+    }
+]
